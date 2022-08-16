f253ba762a9ec8812345e3fae2cb5f9ba67515e3
Christer Weinigel <christer@weinigel.se>
2022-08-16 09:39:16 +0000

    hardcode ecosystem.json in C if no parameters are passed to vrt_client_unix

--- examples/compile.sh
+++ examples/compile.sh
@@ -12,6 +12,7 @@ $CC -Wall -g -DDEBUG -c ../vrt.c -o vrt.o
 $CC -I ../ -Wall -g -DDEBUG -o client vrt_client_unix.c vrt.o tweetnacl.o base64.o
 
 set +e
+./client
 ./client roughtime.weinigel.se 2002 "GdGp9m5AbwqCOpTVYq+yliBIrzh1wed7VYRS5CxMY3U=" 1
 ./client roughtime.cloudflare.com 2002 "gD63hSj3ScS+wuOeGrubXlq35N1c5Lby/S+T7MNTjxo=" 0
 
